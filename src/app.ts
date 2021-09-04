a11fa0c5d9685936a1221749b53ef43803015665
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-09-04 19:23:45 +0430

    New way to bootstrap app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import config from 'config';
 import express from 'express';
-import routes from './routes';
 import db from './utils/connect';
 
 const HOST = config.get<string>('development.host');
@@ -10,13 +9,27 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(PORT, () => {
-    console.log(`App is listening on ${HOST}:${PORT}.`);
-    routes(app);
-    db.authenticate()
-        .then(() => console.log('DB is connected. '))
-        .catch((err) => {
-            console.error(`DB is disconnected. ${err.message}`);
-            process.exit(-1);
+// app.listen(PORT, () => {
+//     console.log(`App is listening on ${HOST}:${PORT}.`);
+//     routes(app);
+//     db.authenticate()
+//         .then(() => console.log('DB is connected. '))
+//         .catch((err) => {
+//             console.error(`DB is disconnected. ${err.message}`);
+//             process.exit(-1);
+//         });
+// });
+
+db.sync()
+    .then(() => {
+        console.log('🔥🔥🔥🔥 DB is connected. 🔥🔥🔥🔥');
+        app.listen(PORT, () => {
+            console.log(`🚀🚀🚀 App is running on ${HOST}:${PORT}.`);
         });
-});
+    })
+    .catch((err: any) => {
+        console.error(`❤️‍🔥❤️‍🔥❤️‍🔥 DB is disconnected. \n${err.message}`);
+    })
+    .finally(() => {
+        db.close();
+    });
